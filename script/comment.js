03b5b01a5ff0dc0a001621e2cae48f4f0a841ffa
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-02 20:26:33 +0900

    fix 수정 버튼 추가

---
+++ script/comment.js
@@ -0,0 +1,91 @@
+let openCloseBtn = document.querySelector('.openClose');
+let inputBtn = document.querySelector('.inputBtn');
+let deleteBtn = document.querySelectorAll('.deleteBtn');
+let commentCount = document.querySelector('.count');
+let commentCard = document.querySelectorAll('.commentCard');
+let editBtn = document.querySelectorAll('.editBtn');
+
+// 삭제 버튼
+for (let i = 0; i < deleteBtn.length; i++) {
+  deleteBtn[i].addEventListener('click', () => {
+    // let passwordInput = prompt('비밀번호를 입력해주세요.', '');
+    let commentPassword = document.querySelectorAll('#commentPassword');
+    if (Number(commentPassword[i].value) === 1234) {
+      alert('비밀번호가 같습니다.');
+      commentPassword[i].value = '';
+    } else {
+      alert('비밀번호가 다릅니다.');
+    }
+  });
+}
+
+// 수정 버튼
+for (let i = 0; i < editBtn.length; i++) {
+  editBtn[i].addEventListener('click', () => {
+    let commentPassword = document.querySelectorAll('#commentPassword');
+    if (Number(commentPassword[i].value) === 1234) {
+      alert('비밀번호가 같습니다.');
+      commentPassword[i].value = '';
+    } else {
+      alert('비밀번호가 다릅니다.');
+    }
+  });
+}
+
+// 댓글 수량 확인
+for (let i = 0; i < commentCard.length; i++) {
+  commentCount.innerText = commentCard.length;
+}
+
+// 댓글 애니메이션 버튼
+openCloseBtn.addEventListener('click', () => {
+  move();
+});
+
+// 댓글 등록 유효성 검사
+inputBtn.addEventListener('click', () => {
+  let userId = document.getElementById('userId');
+  let userPassword = document.getElementById('userPassword');
+  let comment = document.querySelector('.comment');
+  let checkMsg = document.querySelector('.checkMsg');
+
+  if (userId.value.length < 2) {
+    checkMsg.innerText = '아이디는 2글자 이상 입력해주세요.';
+    checkMsg.style.display = 'block';
+  } else if (userPassword.value.length < 6) {
+    checkMsg.innerText = '비밀번호는 6자 이상 입력해주세요.';
+    checkMsg.style.display = 'block';
+  } else if (comment.value === '') {
+    checkMsg.innerText = '내용을 입력해주세요.';
+    checkMsg.style.display = 'block';
+  } else {
+    checkMsg.style.display = 'none';
+    alert('등록 완료');
+    userId.value = '';
+    userPassword.value = '';
+    comment.value = '';
+  }
+});
+
+// 댓글 애니메이션
+let move = () => {
+  let button = document.querySelector('.openClose');
+  let is_CommentBtn = button.getAttribute('data-open') === 'true';
+  button.setAttribute('data-open', !is_CommentBtn);
+  let bodyBox = document.querySelector('.bodyBox');
+  let commentBox = document.querySelector('.commentBox');
+
+  if (is_CommentBtn) {
+    commentBox.style.display = 'block';
+    button.innerText = '>';
+    bodyBox.classList.add('open');
+    bodyBox.classList.remove('close');
+  } else {
+    bodyBox.classList.add('close');
+    bodyBox.classList.remove('open');
+    button.innerText = '<';
+    setTimeout(() => {
+      commentBox.style.display = 'none';
+    }, 1100);
+  }
+};
