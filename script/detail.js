5b3e9ba5bc87c23d574694e64720d3d0222e4d76
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-08 16:33:53 +0900

    style 댓글 UI 디자인 추가

--- script/detail.js
+++ script/detail.js
@@ -5,19 +5,19 @@ let reviewMap = null;
 let deleteButtons = null;
 let updateButtons = null;
 
-const reviewBox = document.querySelector(".review-list");
-const reviewForm = document.querySelector("#reviewForm");
+const reviewBox = document.querySelector('.review-list');
+const reviewForm = document.querySelector('#reviewForm');
 
 const url = new URL(location.href); // 현재 페이지의 url을 url에 저장
 const urlParams = url.searchParams; // urlParams에 현재 url의 파라미터 저장
-const movieId = urlParams.get("id"); // urlParams에서 "id"에 해당하는 값을 가져온다.
+const movieId = urlParams.get('id'); // urlParams에서 "id"에 해당하는 값을 가져온다.
 
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 // localStorage에 저장되어있는 review data를 가져오는 함수
 const getMovieReview = () => {
   // localStorage에 없으면 new Map()으로 만듬
-  reviewMap = localStorage.getItem("review") || new Map();
+  reviewMap = localStorage.getItem('review') || new Map();
 
   // 길이가 있다는것은 데이터가 있다는것
   if (reviewMap.length) {
@@ -28,11 +28,11 @@ const getMovieReview = () => {
 
 // textarea 개행 포멧팅
 const convertText = (text, before, after) => {
-  let output = "";
+  let output = '';
   const arr = text.split(before);
 
   for (let i = 0; i < arr.length; i++) {
-    output += `${arr[i]}${i < arr.length - 1 ? after : ""}`;
+    output += `${arr[i]}${i < arr.length - 1 ? after : ''}`;
   }
 
   return output;
@@ -40,13 +40,13 @@ const convertText = (text, before, after) => {
 // 삭제 및 수정 버튼 클릭 시
 const buttonClickHandler = (buttons, buttonType) => {
   buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
+    btn.addEventListener('click', (e) => {
       // 각 버튼 클릭시 review-id라는 key를 가져옴
-      const key = e.target.getAttribute("review-id");
+      const key = e.target.getAttribute('review-id');
       // 가져온 key로 삭제해야될 li node를 찾음
       const reviewCard = document.querySelector(`li[key=${key}]`);
       // 유저가 입력한 password
-      let inputPassword = reviewCard.querySelector("#review-input-password");
+      let inputPassword = reviewCard.querySelector('#review-input-password');
       let inputPasswordValue = inputPassword.value;
       const thisMap = reviewMap
         .get(movieId)
@@ -55,13 +55,13 @@ const buttonClickHandler = (buttons, buttonType) => {
       const reviewPassword = thisMap.userPasswordValue;
 
       const checkReviewPassword = reviewCard.querySelector(
-        ".review-check-password"
+        '.review-check-password'
       );
 
       if (inputPasswordValue === reviewPassword) {
-        checkReviewPassword.style.display = "none";
-        inputPassword.value = "";
-        if (buttonType === "delete") {
+        checkReviewPassword.style.display = 'none';
+        inputPassword.value = '';
+        if (buttonType === 'delete') {
           // 삭제
           // reviewId와 key가 다른것만 가져옴
           const filteredMap = reviewMap
@@ -71,65 +71,65 @@ const buttonClickHandler = (buttons, buttonType) => {
           // reviewMap에 삭제하고 다시 저장
           reviewMap.set(movieId, filteredMap);
           // localStorage에 삭제하고 다시 저장
-          localStorage.setItem("review", JSON.stringify([...reviewMap]));
+          localStorage.setItem('review', JSON.stringify([...reviewMap]));
           // reviewBox에서 li지워서 새로고침안해도 바로 삭제되는게 보이게함
           reviewBox.removeChild(reviewCard);
 
-          alert("삭제되었습니다.");
+          alert('삭제되었습니다.');
         } else {
           // 수정
           // 리뷰내용과 유효성검사한거 숨김
-          checkReviewPassword.style.display = "none";
+          checkReviewPassword.style.display = 'none';
           const reviewContentBox = reviewCard.querySelector(
-            ".review-content-box"
+            '.review-content-box'
           );
-          reviewContentBox.style.display = "none";
+          reviewContentBox.style.display = 'none';
           const reviewContentValue = thisMap.userCommentValue;
           // 별점 조절할 수 있게 함.
           const reviewUpdateRatingInput = reviewCard.querySelector(
-            ".review-card-rating input"
+            '.review-card-rating input'
           );
           const reviewpdateRatingStar = reviewCard.querySelector(
-            ".review-card-rating .review-star"
+            '.review-card-rating .review-star'
           );
 
-          reviewUpdateRatingInput.addEventListener("input", () => {
+          reviewUpdateRatingInput.addEventListener('input', () => {
             reviewpdateRatingStar.style.width = `${
               reviewUpdateRatingInput.value * 10
             }%`;
           });
           // update box 조립
-          const reviewUpdateBox = document.createElement("div");
-          reviewUpdateBox.className = "review-update-box";
+          const reviewUpdateBox = document.createElement('div');
+          reviewUpdateBox.className = 'review-update-box';
 
-          const reviewUpdateTextarea = document.createElement("textarea");
-          reviewUpdateTextarea.className = "review-update-textarea";
+          const reviewUpdateTextarea = document.createElement('textarea');
+          reviewUpdateTextarea.className = 'review-update-textarea';
           reviewUpdateTextarea.value = convertText(
             reviewContentValue,
-            "<br/>",
-            "\n"
+            '<br/>',
+            '\n'
           );
-          const textareaRow = reviewUpdateTextarea.value.split("\n").length;
+          const textareaRow = reviewUpdateTextarea.value.split('\n').length;
           reviewUpdateTextarea.rows = textareaRow;
-          reviewUpdateTextarea.addEventListener("input", () => {
-            reviewUpdateTextarea.style.height = "auto";
+          reviewUpdateTextarea.addEventListener('input', () => {
+            reviewUpdateTextarea.style.height = 'auto';
             reviewUpdateTextarea.style.height =
               reviewUpdateTextarea.scrollHeight + `px`;
           });
-          const reviewButtonBox = document.createElement("div");
-          reviewButtonBox.className = "review-button-box";
+          const reviewButtonBox = document.createElement('div');
+          reviewButtonBox.className = 'review-button-box';
 
-          const reviewButtonUpdate = document.createElement("button");
-          reviewButtonUpdate.className = "review-button-update";
-          reviewButtonUpdate.type = "button";
-          reviewButtonUpdate.innerText = "확인";
-          reviewButtonUpdate.addEventListener("click", () => {
+          const reviewButtonUpdate = document.createElement('button');
+          reviewButtonUpdate.className = 'review-button-update';
+          reviewButtonUpdate.type = 'button';
+          reviewButtonUpdate.innerText = '확인';
+          reviewButtonUpdate.addEventListener('click', () => {
             const reviewContent =
-              reviewContentBox.querySelector(".review-content");
+              reviewContentBox.querySelector('.review-content');
             reviewContent.innerHTML = convertText(
               reviewUpdateTextarea.value,
-              "\n",
-              "<br/>"
+              '\n',
+              '<br/>'
             );
 
             reviewMap
@@ -140,34 +140,34 @@ const buttonClickHandler = (buttons, buttonType) => {
             reviewMap
               .get(movieId)
               .find((data) => data.reviewId === key).userCommentValue =
-              convertText(reviewUpdateTextarea.value, "<br/>", "\n");
+              convertText(reviewUpdateTextarea.value, '<br/>', '\n');
 
-            localStorage.setItem("review", JSON.stringify([...reviewMap]));
-            reviewContentBox.style.display = "block";
+            localStorage.setItem('review', JSON.stringify([...reviewMap]));
+            reviewContentBox.style.display = 'block';
             reviewCard.removeChild(reviewUpdateBox);
           });
 
-          const reviewButtonCancel = document.createElement("button");
-          reviewButtonCancel.className = "review-button-delete";
-          reviewButtonCancel.type = "button";
-          reviewButtonCancel.innerText = "취소";
-          reviewButtonCancel.addEventListener("click", () => {
-            reviewContentBox.style.display = "block";
+          const reviewButtonCancel = document.createElement('button');
+          reviewButtonCancel.className = 'review-button-delete';
+          reviewButtonCancel.type = 'button';
+          reviewButtonCancel.innerText = '취소';
+          reviewButtonCancel.addEventListener('click', () => {
+            reviewContentBox.style.display = 'block';
             reviewCard.removeChild(reviewUpdateBox);
           });
 
           reviewButtonBox.append(reviewButtonUpdate, reviewButtonCancel);
           reviewUpdateBox.append(reviewUpdateTextarea, reviewButtonBox);
-          reviewCard.insertAdjacentElement("beforeend", reviewUpdateBox);
+          reviewCard.insertAdjacentElement('beforeend', reviewUpdateBox);
 
           // reviewContent.textContent = input.value;
         }
       } else if (!inputPasswordValue.length) {
-        checkReviewPassword.innerText = "비밀번호를 입력해주세요.";
-        checkReviewPassword.style.display = "block";
+        checkReviewPassword.innerText = '비밀번호를 입력해주세요.';
+        checkReviewPassword.style.display = 'block';
       } else {
-        checkReviewPassword.innerText = "비밀번호가 다릅니다.";
-        checkReviewPassword.style.display = "block";
+        checkReviewPassword.innerText = '비밀번호가 다릅니다.';
+        checkReviewPassword.style.display = 'block';
       }
     });
   });
@@ -185,7 +185,6 @@ const createReview = ({
     <li key=${reviewId} class="review-card">
       <div class="review-card-info">
         <div class="review-card-info-user">
-          <h4 id="userName">${userIdValue}</h4>
           <div class="review-rating review-card-rating">
             ★★★★★
             <span style="width:${
@@ -193,6 +192,11 @@ const createReview = ({
             }%" class="review-star">★★★★★</span>
             <input type="range" value="1" step="1" min="0" max="10" />
           </div>
+          <div class="review-name-edit">
+          <h4 id="userName">${userIdValue}</h4>
+          <button review-id=${reviewId} class="review-button-edit">편집</button>
+          </div>
+          
         </div>
         <div class="review-card-info-password">
           <input
@@ -208,8 +212,8 @@ const createReview = ({
         <div class="review-content-main">
           <p class="review-content">${convertText(
             userCommentValue,
-            "\n",
-            "<br/>"
+            '\n',
+            '<br/>'
           )}</p>
         </div>
         <div class="review-button-box">
@@ -224,17 +228,17 @@ const createReview = ({
   reviewBox.innerHTML += html;
 
   // 생성할때마다 삭제, 수정버튼을 등록하기 위해 매번 다시 가져옴
-  deleteButtons = [...document.querySelectorAll(".review-button-delete")];
-  updateButtons = [...document.querySelectorAll(".review-button-update")];
+  deleteButtons = [...document.querySelectorAll('.review-button-delete')];
+  updateButtons = [...document.querySelectorAll('.review-button-update')];
 
-  buttonClickHandler(deleteButtons, "delete");
-  buttonClickHandler(updateButtons, "update");
+  buttonClickHandler(deleteButtons, 'delete');
+  buttonClickHandler(updateButtons, 'update');
 };
 
 // DOM이 만들어 진 후 실행되는 함수
-document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("mode") !== "dark") {
-    body.classList.add("light");
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('mode') !== 'dark') {
+    body.classList.add('light');
   } // 페이지 진입 시 화면 모드 결정
 
   getMovieReview();
@@ -244,26 +248,26 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // 별점
-  const reviewFormInput = document.querySelector(".review-form-rating input");
+  const reviewFormInput = document.querySelector('.review-form-rating input');
   const reviewFormStar = document.querySelector(
-    ".review-form-rating .review-star"
+    '.review-form-rating .review-star'
   );
-  reviewFormInput.addEventListener("input", () => {
+  reviewFormInput.addEventListener('input', () => {
     reviewFormStar.style.width = `${reviewFormInput.value * 10}%`;
   });
 });
 
 // submit 이벤트 발생시 리뷰 등록하는 함수
-reviewForm.addEventListener("submit", (e) => {
+reviewForm.addEventListener('submit', (e) => {
   // 새로고침 방지
   e.preventDefault();
 
-  let userId = document.querySelector("#userId");
-  let userPassword = document.querySelector("#userPassword");
-  let userComment = document.querySelector("#userComment");
-  let checkMsg = document.querySelector(".checkMsg");
+  let userId = document.querySelector('#userId');
+  let userPassword = document.querySelector('#userPassword');
+  let userComment = document.querySelector('#userComment');
+  let checkMsg = document.querySelector('.checkMsg');
   // 별점
-  const reviewRating = document.querySelector(".review-form-rating");
+  const reviewRating = document.querySelector('.review-form-rating');
   const reviewRatingInput = reviewRating.lastElementChild;
 
   const userRatingValue = reviewRatingInput.value;
@@ -273,25 +277,25 @@ reviewForm.addEventListener("submit", (e) => {
 
   // 댓글 등록 유효성 검사
   if (userIdValue.length < 2) {
-    checkMsg.innerText = "아이디는 2글자 이상 입력해주세요.";
-    checkMsg.style.display = "block";
+    checkMsg.innerText = '아이디는 2글자 이상 입력해주세요.';
+    checkMsg.style.display = 'block';
     return null;
   } else if (userPasswordValue.length < 6) {
-    checkMsg.innerText = "비밀번호는 6자 이상 입력해주세요.";
-    checkMsg.style.display = "block";
+    checkMsg.innerText = '비밀번호는 6자 이상 입력해주세요.';
+    checkMsg.style.display = 'block';
     return null;
   } else if (!userCommentValue.length) {
-    checkMsg.innerText = "내용을 입력해주세요.";
-    checkMsg.style.display = "block";
+    checkMsg.innerText = '리뷰을 작성해주세요.';
+    checkMsg.style.display = 'block';
     return null;
   } else {
-    checkMsg.style.display = "none";
+    checkMsg.style.display = 'none';
   }
 
   // movieId를 key로써 map에 접근해서 데이터 가져옴
   const map = reviewMap.get(movieId);
 
-  let reviewId = "";
+  let reviewId = '';
   // reviewId는 랜덤한 값으로 설정함
   // 근데 li의 key로써 쓰려면 숫자로 시작하면 안됨
   // 그래서 숫자로 시작하는지 do-while 돌면서 확인함
@@ -321,12 +325,12 @@ reviewForm.addEventListener("submit", (e) => {
 
   // localStorage에 data 저장
   // localStorage는 js객체를 저장못함, 그래서 저장할수있게 변경해야함
-  localStorage.setItem("review", JSON.stringify([...reviewMap]));
+  localStorage.setItem('review', JSON.stringify([...reviewMap]));
 
   // 저장 후 input들 비워줌
-  userId.value = "";
-  userPassword.value = "";
-  userComment.value = "";
+  userId.value = '';
+  userPassword.value = '';
+  userComment.value = '';
   reviewRatingInput.value = 0;
 });
 
@@ -336,6 +340,6 @@ function backSpace() {
 }
 
 // 뒤로가기 기능
-document.querySelector(".backBtn").addEventListener("click", function () {
+document.querySelector('.backBtn').addEventListener('click', function () {
   backSpace();
 });
